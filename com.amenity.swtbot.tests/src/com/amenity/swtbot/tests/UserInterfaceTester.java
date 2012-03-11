0f6b7ee872ba99d01476b1c3994acb2283086927
Ben Riedel <ben@go-unified.com>
2012-03-12 00:57:26 +0100

    swt

---
+++ com.amenity.swtbot.tests/src/com/amenity/swtbot/tests/UserInterfaceTester.java
@@ -0,0 +1,13 @@
+package com.amenity.swtbot.tests;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class UserInterfaceTester {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+}
